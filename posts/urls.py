fe2692aec747b4fd240a18953cfb306a2c7ae110
faridhaavasi <faridhavasi777@gmail.com>
2023-07-30 17:53:21 +0330

    debug 1 Add mixins

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('detail/<int:pk>', views.DetailPostApi.as_view(), name='detail'),
-    path('posts', views.PostApi.as_view(), name='posts'),
+    path('posts', views.PostsmixinsApi.as_view(), name='post'),
+    path('detail/posts/<int:pk>', views.DetailPostMixinApi.as_view(), name='detail'),
 
 ]
--- posts/views.py
+++ posts/views.py
@@ -3,28 +3,67 @@ from rest_framework.response import Response
 from .models import Post
 from .serializers import Postserializer
 from rest_framework.permissions import IsAuthenticated
-class PostApi(APIView):
-    permission_classes = [IsAuthenticated]
+from rest_framework import generics, mixins
+
+# region cbv
+# class PostApi(APIView):
+#     permission_classes = [IsAuthenticated]
+#     def get(self, request):
+#         queryset = Post.objects.all()
+#         serializer = Postserializer(instance=queryset, many=True)
+#         return Response(serializer.data)
+#     def post(self, request):
+#         serializer = Postserializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.validated_data['user'] = request.user
+#             serializer.save()
+#             return Response({'massage': 'added'})
+#         return Response(serializer.errors)
+#     def delete(self, request, pk):
+#         instance = Post.objects.get(pk=pk)
+#         instance.delete()
+#         return Response({'massage': 'deleted'})
+#
+#
+# class DetailPostApi(APIView):
+#     def get(self, request, pk):
+#         instance = Post.objects.get(pk=pk)
+#         serializer = Postserializer(instance=instance)
+#         return Response(serializer.data)
+#
+# endregion
+
+'''
+Create, update and delete operations are implemented by mixins
+'''
+
+
+# region mixin
+class PostsmixinsApi(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Post.objects.all()
+    serializer_class = Postserializer
+
     def get(self, request):
-        queryset = Post.objects.all()
-        serializer = Postserializer(instance=queryset, many=True)
-        return Response(serializer.data)
+        return self.list(request)
+
     def post(self, request):
-        serializer = Postserializer(data=request.data)
-        if serializer.is_valid():
-            serializer.validated_data['user'] = request.user
-            serializer.save()
-            return Response({'massage': 'added'})
-        return Response(serializer.errors)
-    def delete(self, request, pk):
-        instance = Post.objects.get(pk=pk)
-        instance.delete()
-        return Response({'massage': 'deleted'})
+        return self.create(request)
 
 
-class DetailPostApi(APIView):
+class DetailPostMixinApi(mixins.RetrieveModelMixin,
+                         mixins.DestroyModelMixin,
+                         mixins.UpdateModelMixin, generics.GenericAPIView):
+    queryset = Post.objects.all()
+
+    serializer_class = Postserializer
+
     def get(self, request, pk):
-        instance = Post.objects.get(pk=pk)
-        serializer = Postserializer(instance=instance)
-        return Response(serializer.data)
+        return self.retrieve(request, pk)
 
+    def put(self, request, pk):
+        return self.update(request, pk)
+
+    def delete(self, request, pk):
+        return self.destroy(request, pk)
+
+# endregion
